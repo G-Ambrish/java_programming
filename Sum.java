0f8c75b39611c0f6b4621018b8e16a5afcc66a3d
G-Ambrish <61139737+G-Ambrish@users.noreply.github.com>
2020-02-17 14:48:17 +0530

    Create Sum.java

---
+++ Sum.java
@@ -0,0 +1,19 @@
+//Write a Program to find sum of 2 integers.
+
+class Sum
+
+    {
+
+        public static void main(String args[])
+
+        {
+
+            int a=5,b=6,sum;
+
+            sum=a+b;
+
+            System.out.println("Sum="+sum);
+
+        }
+
+    }
