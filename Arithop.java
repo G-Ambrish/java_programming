8ee0be11d793ac912cf4dda6aa8bb307a585bd57
G-Ambrish <61139737+G-Ambrish@users.noreply.github.com>
2020-02-17 14:51:30 +0530

    Create Arithop.java

---
+++ Arithop.java
@@ -0,0 +1,33 @@
+//Write a java program to perform arithmetic operations
+
+    
+    class Arithop
+
+    {
+
+        public static void main(String args[])
+
+        {
+
+            int a=5,b=6,Sum,Prod,Sub,Div;
+
+            Sum=a+b;
+
+            System.out.println("Sum="+Sum);
+
+            Prod=a*b;
+
+            System.out.println("Product="+Prod);
+
+            Sub=a-b;
+
+            System.out.println("Sub="+Sub);
+
+            Div=a/b;
+
+            System.out.println("Div="+Div);
+
+        }
+
+    }
+
